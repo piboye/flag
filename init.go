a27ddc1da95536e9aef64b68d67ff6b864a5eab9
piboyeliu <piboyeliu@tencent.com>
2021-03-24 15:23:35 +0800

    fix init

--- init.go
+++ init.go
@@ -31,11 +31,12 @@ func init() {
 	if CommandLine.values == nil {
 		CommandLine.values = make(map[string]string)
 	}
+
 	values := CommandLine.values
-	CommandLine.preParse(os.Args[1:])
 
 	preParseFile(values)
 	preParseEnv(values)
+	CommandLine.preParse(os.Args[1:])
 
 	/*
 		for _, arg := range os.Args[1:] {
